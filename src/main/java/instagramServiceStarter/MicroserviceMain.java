da3a4b48a13750e6481c0bfeb330c6fe5c8f938b
Alex <arpearce93@gmail.com>
2019-05-26 00:04:54 +0100

    Spring boot setup

---
+++ src/main/java/instagramServiceStarter/MicroserviceMain.java
@@ -0,0 +1,12 @@
+package instagramServiceStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMain.class, args);
+    }
+}
